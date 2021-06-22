4e84c28be4ee9ddd9fe528646c54f257996ffa9f
VoidConcept <voidconception@gmail.com>
2021-06-21 21:48:18 -0700

    Only fetch one level of quests

--- src/index.ts
+++ src/index.ts
@@ -5,37 +5,75 @@ const isHtmlElement = (child: Node): child is HTMLElement => {
     return (child as HTMLElement).nodeType === NodeType.ELEMENT_NODE
 }
 
-const getNested = (htmlElement: HTMLElement) => {
-    const link = htmlElement.querySelector("a")
+const isUlHtmlElement = (child: Node): child is HTMLElement => {
+    return isHtmlElement(child) && child.rawTagName === "ul"
+}
+
+const isLiHtmlElement = (child: Node): child is HTMLElement => {
+    return isHtmlElement(child) && child.rawTagName === "li"
+}
+
+type Quest = {
+    questName: string
+    href: string
+}
+type QuestList = Quest[]
+
+const scrapeQuestRequirements = (document: HTMLElement): QuestList => {
+    const questReq = document.querySelector(".questreq")
+    const trTd = questReq.querySelector("tr td")
+    const htmlElement = trTd.querySelector("ul li")
 
     const children = htmlElement.childNodes
-        .filter(isHtmlElement)
-        .filter(child => {
-            return child.rawTagName === 'ul'
-        })
-        .map(child => child.childNodes.filter(isHtmlElement).filter(child => child.rawTagName === 'li'))
+        .filter(isUlHtmlElement)
+        .map(child => child.childNodes.filter(isLiHtmlElement))
         .reduce((acc, val) => acc.concat(val), [])
-        .map(elem => getNested(elem));
 
+    return children.map(child => {
+        //remove ul elements to not pollute names
+        child.childNodes.filter(isUlHtmlElement).map(nestedChild => child.removeChild(nestedChild))
+
+        const link = child.querySelector("a")
+        return {
+            questName: child.innerText,
+            href: link.getAttribute("href")
+        }
+    })
+}
+
+type FetchedQuest = {
+    questName: string,
+    href: string,
+    questRequirements: QuestList
+}
+
+const fetchDocument = async (url: string): Promise<HTMLElement> => {
+    const response = await fetch(url)
+    const htmlText = await response.text()
+    return await parse(htmlText)
+}
+
+const fetchQuest = async (quest: Quest): Promise<FetchedQuest> => {
+    const document = await fetchDocument(`https://runescape.wiki${quest.href}`)
+
+    const questRequirements = scrapeQuestRequirements(document)
     return {
-        questName: link.innerText,
-        link: link.getAttribute("href"),
-        children: children
+        ...quest,
+        questRequirements
     }
 }
 
+const fetchAllQuests = async () => {
+    const document = await fetchDocument("https://runescape.wiki/w/List_of_quests")
+}
+
 const run = async () => {
-    const response = await fetch("https://runescape.wiki/w/While_Guthix_Sleeps")
-    const htmlText = await response.text()
+    const fetchedQuest = await fetchQuest({
+        questName: "Legend's Quest",
+        href: "/w/Legends%27_Quest"
+    })
 
-    const document = await parse(htmlText)
-
-    const questReq = document.querySelector(".questreq")
-    const trTd = questReq.querySelector("tr td")
-
-    const list = getNested(trTd.querySelector("ul li"))
-
-    console.log(JSON.stringify(list))
+    console.log(JSON.stringify(fetchedQuest))
 }
 
 

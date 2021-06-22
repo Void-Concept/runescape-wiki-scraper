efd61c127efa1cd31348061e6448100bd2fd2b94
VoidConcept <voidconception@gmail.com>
2021-06-21 22:17:09 -0700

    Fetch quest list

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,8 @@ type QuestList = Quest[]
 
 const scrapeQuestRequirements = (document: HTMLElement): QuestList => {
     const questReq = document.querySelector(".questreq")
+    if (!questReq) return []
+
     const trTd = questReq.querySelector("tr td")
     const htmlElement = trTd.querySelector("ul li")
 
@@ -63,17 +65,64 @@ const fetchQuest = async (quest: Quest): Promise<FetchedQuest> => {
     }
 }
 
+type QuestDifficulty = 'Novice' | 'Intermediate' | 'Experienced' | 'Master' | 'Grandmaster' | 'Special'
+
+type QuestAge = 'Fifth Age' | 'Sixth Age' | 'Ambiguous'
+
+type QuestListQuest = {
+    name: string
+    href: string
+    members: boolean
+    difficulty: QuestDifficulty
+    length: string
+    age: QuestAge
+    questPoints: number
+    series: string
+}
+
+const scrapeQuestList = (document: HTMLElement): QuestListQuest[] => {
+    const getText = (cell: HTMLElement): string => {
+        return cell.innerText.replace("\n", '')
+    }
+
+    const tableBody = document.querySelector("table.wikitable.sortable tbody")
+    const rowsWithHeader = tableBody.querySelectorAll("tr")
+    const rows = rowsWithHeader.slice(1)
+    const quests = rows.map(row => {
+        const cells = row.querySelectorAll("td")
+
+        const [nameCell, membersCell, difficultCell, lengthCell, ageCell, questPointsCell, seriesCell] = cells
+
+        return {
+            name: getText(nameCell),
+            href: nameCell.querySelector("a").getAttribute("href"),
+            members: !!membersCell.innerText.match(/.*Yes.*/),
+            difficulty: getText(difficultCell) as QuestDifficulty,
+            length: getText(lengthCell),
+            age: getText(ageCell) as QuestAge,
+            questPoints: parseInt(getText(questPointsCell)),
+            series: getText(seriesCell)
+        }
+    })
+    return quests
+}
+
 const fetchAllQuests = async () => {
     const document = await fetchDocument("https://runescape.wiki/w/List_of_quests")
+
+    return scrapeQuestList(document)
 }
 
 const run = async () => {
-    const fetchedQuest = await fetchQuest({
-        questName: "Legend's Quest",
-        href: "/w/Legends%27_Quest"
-    })
+    // const fetchedQuest = await fetchQuest({
+    //     questName: "Legend's Quest",
+    //     href: "/w/Legends%27_Quest"
+    // })
 
-    console.log(JSON.stringify(fetchedQuest))
+    // console.log(JSON.stringify(fetchedQuest))
+
+    const quests = await fetchAllQuests()
+    console.log(quests)
 }
 
 
